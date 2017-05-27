7906b4a06bb4b246e00eccf8ce92aebe7d8e51da
Dimitri van Leent <dimi82@gmail.com>
2017-05-27 11:10:59 +0200

    appcomponent uit module verwijderd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
 import { SquareComponent } from './square/square.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
     BoardComponent,
     SquareComponent
   ],
